d22857df9ca204489e869e250cd91aac2fce8db9
melvin <melvin.mark@centrino.co.ke>
2022-10-15 15:24:19 +0300

    fix: eradicate use of magic strings

---
+++ Constants/CalculatorComponents.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+
+namespace OnlineCalculator.Constants;
+
+public enum CalculatorComponents
+{
+    [Description("+")]
+    Addition,
+    [Description("-")]
+    Subtraction,
+    [Description("⨉")]
+    Multiplication,
+    [Description("÷")]
+    Division,
+    [Description("C")]
+    Clear,
+    [Description("=")]
+    Equal,
+    [Description(".")]
+    DecimalPoint,
+    [Description("%")]
+    Modulus,
+    [Description("x²")]
+    Exponential,
+    [Description("7")]
+    Seven,
+    [Description("8")]
+    Eight,
+    [Description("9")]
+    Nine,
+    [Description("4")]
+    Four,
+    [Description("5")]
+    Five,
+    [Description("6")]
+    Six,
+    [Description("1")]
+    One,
+    [Description("2")]
+    Two,
+    [Description("3")]
+    Three,
+    [Description("0")]
+    Zero,
+  
+
+
+}
---
+++ Extensions/EnumerationUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace OnlineCalculator.Extensions;
+
+public static class EnumerationUtility
+{
+    public static string GetEnumDescription(Enum value)
+    {
+        FieldInfo? fi = value.GetType().GetField(value.ToString());
+
+        if (fi?.GetCustomAttributes(typeof(DescriptionAttribute), false) is DescriptionAttribute[] attributes && attributes.Any())
+        {
+            return attributes.First().Description;
+        }
+
+        return value.ToString();
+    }
+}
